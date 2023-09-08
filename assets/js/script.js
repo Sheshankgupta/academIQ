4261d47a7cdff1c1f8833e913039f3128e465238
Sheshankgupta <--global>
2023-09-08 13:16:53 +0530

    changes with navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,3 +13,16 @@ function scrollFunction() {
     document.getElementById("navCompact").style.pointerEvents = "none";
   }
 }
+
+// document.addEventListener("DOMContentLoaded", function (){
+//   const arr = ['rgba(190, 190, 190, 0.3)', 'rgba(200, 200, 200, 0.4)', 'rgba(210, 210, 210, 0.5)', 'rgba(220, 220, 220, 0.6)']
+//   let i = 0;
+//   window.setInterval(function () {
+//     console.log(i)
+//     document.getElementById("descriptions").style.backgroundColor = arr[i];
+//     i++;
+//     if(i === arr.length){
+//       i = 0;
+//     }
+//   }, 1000);
+// });
